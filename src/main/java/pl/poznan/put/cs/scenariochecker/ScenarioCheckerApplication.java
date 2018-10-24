6fb95a2002de43a37741081709980a08d48fb7cb
Damian Horna <damian.horna@interia.pl>
2018-10-24 13:37:20 +0200

    Initial commit

---
+++ src/main/java/pl/poznan/put/cs/scenariochecker/ScenarioCheckerApplication.java
@@ -0,0 +1,12 @@
+package pl.poznan.put.cs.scenariochecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenarioCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScenarioCheckerApplication.class, args);
+    }
+}
