bebaf9e06f3844a4bd4205d8a2883077b54e951a
plutasnyy <kamil.plucinski97@gmail.com>
2018-10-27 13:26:25 +0200

    remove unnecessary setters and getters

--- src/main/java/pl/poznan/put/cs/scenariochecker/model/Scenario.java
+++ src/main/java/pl/poznan/put/cs/scenariochecker/model/Scenario.java
@@ -10,34 +10,5 @@ public class Scenario {
     private String systemActor;
     private List<Step> steps;
     private List<String> actors;
-
     private int numberOfSteps;
-
-    public void setNumberOfSteps(int numberOfSteps) {
-        this.numberOfSteps = numberOfSteps;
-    }
-
-    public void setSteps(List<Step> steps) {
-        this.steps = steps;
-    }
-
-    public int getNumberOfSteps() {
-        return numberOfSteps;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public List<String> getActors() {
-        return actors;
-    }
-
-    public String getSystemActor() {
-        return systemActor;
-    }
-
-    public List<Step> getSteps() {
-        return steps;
-    }
 }
